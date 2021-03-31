07a8bed2758c56b2cf1eb6ff08cd520d3ca793aa
alex <alex.kudelko@gmail.com>
2021-03-31 22:52:15 +0300

    dz 18

--- academy/tests/test_models.py
+++ academy/tests/test_models.py
@@ -15,15 +15,13 @@ class StudentModelTest(TestCase):
         cls.lecturer = Lecturer.objects.create(first_name=cls.first_name,
                                                last_name=cls.last_name,
                                                email=cls.email,
-                                               photo=cls.photo
+                                               photo=cls.photo,
                                                )
-        cls.lecturer.save()
         cls.student = Student.objects.create(first_name=cls.first_name,
                                              last_name=cls.last_name,
                                              email=cls.email,
                                              photo=cls.photo
                                              )
-        cls.lecturer.save()
 
     def setUp(self) -> None:
         # вызывается перед каждой тестовой функцией для настройки объектов,
@@ -41,6 +39,50 @@ class StudentModelTest(TestCase):
                           )
         student.full_clean()
 
+    def test_failure_due_to_student_long_first_name(self):
+        long_first_name = 'a' * 31
+        student = Student(first_name=long_first_name,
+                          last_name=self.last_name,
+                          email=self.email,
+                          photo=self.photo
+                          )
+        expected_message = 'Ensure this value has at most 30 characters (it has 31).'
+        with self.assertRaisesMessage(ValidationError, expected_message):
+            student.full_clean()
+
+    def test_failure_due_to_student_long_last_name(self):
+        long_last_name = 'a' * 31
+        student = Student(first_name=self.first_name,
+                          last_name=long_last_name,
+                          email=self.email,
+                          photo=self.photo
+                          )
+        expected_message = 'Ensure this value has at most 30 characters (it has 31).'
+        with self.assertRaisesMessage(ValidationError, expected_message):
+            student.full_clean()
+
+    def test_failure_due_to_student_long_valid_email(self):
+        long_email = f"{'a' * 42}@mail.com"
+        email = Student(first_name=self.first_name,
+                        last_name=self.last_name,
+                        email=long_email,
+                        photo=self.photo
+                        )
+        expected_message = 'Ensure this value has at most 50 characters (it has 51).'
+        with self.assertRaisesMessage(ValidationError, expected_message):
+            email.full_clean()
+
+    def test_failure_due_to_student_long_not_valid_email(self):
+        long_email = 'a' * 51
+        email = Student(first_name=self.first_name,
+                        last_name=self.last_name,
+                        email=long_email,
+                        photo=self.photo
+                        )
+        expected_messages = 'Enter a valid email address.'
+        with self.assertRaisesMessage(ValidationError, expected_messages):
+            email.full_clean()
+
     def test_successfully_lecturer_creation(self):
         student = Lecturer(first_name=self.first_name,
                            last_name=self.last_name,
@@ -49,37 +91,75 @@ class StudentModelTest(TestCase):
                            )
         student.full_clean()
 
+    def test_failure_due_to_lecturer_long_first_name(self):
+        long_first_name = 'a' * 31
+        lecturer = Lecturer(first_name=long_first_name,
+                            last_name=self.last_name,
+                            email=self.email,
+                            photo=self.photo
+                            )
+        expected_message = 'Ensure this value has at most 30 characters (it has 31).'
+        with self.assertRaisesMessage(ValidationError, expected_message):
+            lecturer.full_clean()
+
+    def test_failure_due_to_lecturer_long_last_name(self):
+        long_last_name = 'a' * 31
+        lecturer = Lecturer(first_name=self.first_name,
+                            last_name=long_last_name,
+                            email=self.email,
+                            photo=self.photo
+                            )
+        expected_message = 'Ensure this value has at most 30 characters (it has 31).'
+        with self.assertRaisesMessage(ValidationError, expected_message):
+            lecturer.full_clean()
+
+    def test_failure_due_to_lecturer_long_valid_email(self):
+        long_email = f"{'a' * 42}@mail.com"
+        email = Lecturer(first_name=self.first_name,
+                         last_name=self.last_name,
+                         email=long_email,
+                         photo=self.photo
+                         )
+        expected_message = 'Ensure this value has at most 50 characters (it has 51).'
+        with self.assertRaisesMessage(ValidationError, expected_message):
+            email.full_clean()
+
+    def test_failure_due_to_lecturer_long_not_valid_email(self):
+        long_email = 'a' * 51
+        email = Lecturer(first_name=self.first_name,
+                         last_name=self.last_name,
+                         email=long_email,
+                         photo=self.photo
+                         )
+        expected_messages = 'Enter a valid email address.'
+        with self.assertRaisesMessage(ValidationError, expected_messages):
+            email.full_clean()
+
     def test_successfully_group_creation(self):
-        group = Group(course="Python",
-                      group_name="test_group"
-                      )
-
+        group = Group.objects.create(course="Python",
+                                     group_name="test_group"
+                                     )
         group.teachers.add(self.lecturer)
-
         group.students.add(self.student)
+        # validiruet ne sozdanie obekti
+        # group.full_clean()
 
-        # # group.save()
-        group.full_clean()
+    def test_failure_due_to_group_long_course_field(self):
+        long_course = 'a' * 31
+        group = Group.objects.create(course=long_course,
+                                     group_name="test_group"
+                                     )
 
-    # def test_successfully_group_creation(self):
-    #     group = Group(course="Python",
-    #                   group_name="test_group"
-    #                   )
-    #     lecturer = Lecturer.objects.create(first_name=self.first_name,
-    #                                        last_name=self.last_name,
-    #                                        email=self.email,
-    #                                        photo=self.photo
-    #                                        )
-    #     lecturer.save()
-    #     group.teachers.add(lecturer)
-    #     student = Student.objects.create(first_name=self.first_name,
-    #                                      last_name=self.last_name,
-    #                                      email=self.email,
-    #                                      photo=self.photo
-    #                                      )
-    #
-    #     student.save()
-    #     group.students.add(student)
-    #
-    #     # # group.save()
-    #     group.full_clean()
+        expected_message = 'Ensure this value has at most 30 characters (it has 31).'
+        with self.assertRaisesMessage(ValidationError, expected_message):
+            group.full_clean()
+
+    def test_failure_due_to_group_long_group_name(self):
+        long_group_name = 'a' * 21
+        group = Group.objects.create(course="Python",
+                                     group_name=long_group_name
+                                     )
+
+        expected_message = 'Ensure this value has at most 20 characters (it has 21).'
+        with self.assertRaisesMessage(ValidationError, expected_message):
+            group.full_clean()
---
+++ academy/tests/test_views.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
+
+from academy.models import Group, Lecturer, Student
+
+
+class StudentListViewTest(TestCase):
+    NUMBER_OF_STUDENT = 10
+    NUMBER_OF_GROUP = 10
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.first_name = "Jhon"
+        cls.last_name = "Doe"
+        cls.email = "jdoe@fdsdf.com"
+        cls.photo = "photo/default.png"
+        cls.lecturer = Lecturer.objects.create(first_name=cls.first_name,
+                                               last_name=cls.last_name,
+                                               email=cls.email,
+                                               photo=cls.photo,
+                                               )
+        cls.student = Student.objects.create(first_name=cls.first_name,
+                                             last_name=cls.last_name,
+                                             email=cls.email,
+                                             photo=cls.photo
+                                             )
+        for number in range(cls.NUMBER_OF_GROUP):
+            group = Group.objects.create(course="Python",
+                                         group_name=f"test_group{number}"
+                                         )
+            group.teachers.add(cls.lecturer)
+            for _ in range(cls.NUMBER_OF_STUDENT):
+                group.students.add(cls.student)
+
+    def test_view_students_url_exists_at_desired_location(self):
+        resp = self.client.get('/students')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_students_url_accessible_by_name(self):
+        resp = self.client.get(reverse('students'))
+        self.assertEqual(resp.status_code, 200)
